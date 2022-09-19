adcb8bd2cb16840e8993e7fd292a2f478def7178
Olimpo Macea <olimpo3m@gmail.com>
2022-09-19 12:13:46 -0500

    Creando la barra de navegación

---
+++ src/components/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container ">
+  <H1>Transportes Ruiz</H1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MenuPrincipal.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container ">
+    <ul class="nav justify-content-end">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#"
+          >Quienes Somos</a
+        >
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Servicios</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contactenos</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+</style>
